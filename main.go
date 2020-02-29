2ed17f3a065511f1ce1681f51c9f8b6a351d59a0
EricKIm <kimhi65@gmail.com>
2020-02-29 22:25:12 +0900

    200229-2225-Kim

--- main.go
+++ main.go
@@ -1,49 +1,13 @@
 package main
 
 import (
-
-	// grp1 "end-kfgrpc/protos"
-	// "time"
-
 	"github.com/dabory/abango"
-	// "google.golang.org/grpc"
-	// "google.golang.org/grpc/keepalive"
 )
 
 func init() {
-	abango.RunEndRequest() //Should be here before gRpcServer Run
+	abango.RunEndRequest()
 }
 
 func main() {
 
 }
-
-// func gRpcRequest(askname string, askstr string) (string, string) {
-
-// 	dial, err := grpc.Dial(
-// 		cf.XConfig["gRpcAddr"]+":"+cf.XConfig["gRpcPort"],
-// 		grpc.WithInsecure(),
-// 		grpc.WithKeepaliveParams(keepalive.ClientParameters{
-// 			Time:                time.Millisecond,
-// 			Timeout:             time.Millisecond,
-// 			PermitWithoutStream: true,
-// 		}),
-// 	)
-
-// 	if err != nil {
-// 		e.MyErr("WOEIURQPWERQ", nil, true)
-// 	}
-// 	defer dial.Close()
-// 	c := grp1.NewGrp1Client(dial)
-
-// 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-// 	defer cancel()
-
-// 	r, err := c.StdRpc(ctx, &grp1.StdAsk{AskName: askname, AskStr: askstr})
-// 	if err != nil {
-// 		e.MyErr("erttryrtye", err, true)
-// 	}
-
-// 	return r.RetStr, r.RetSta
-
-// }
